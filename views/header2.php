e072061b9ff5baf2f1dcb79b26faf4fd4d0c12d5
Leandro Calvetti <lcalvetti1@gmail.com>
2021-04-27 15:12:57 -0300

    nuevo cabezal

--- views/header2.php
+++ views/header2.php
@@ -32,7 +32,7 @@
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-light border-2" type="submit">Search</button>
+        <button class="btn btn-outline-light" type="submit">Search</button>
       </form>
     </div>
   </div>

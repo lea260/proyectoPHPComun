83decd7b6f6cdf6d920a30bceb793d3d7b08709c
Leandro Calvetti <lcalvetti1@gmail.com>
2021-04-27 14:59:45 -0300

    cambios en header y login

--- views/header2.php
+++ views/header2.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary ps-5">
   <div class="container-fluid">
     <a class="navbar-brand" href="<?php echo constant('URL'); ?>index">Inicio</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,11 +10,11 @@
           <a class="nav-link active" aria-current="page" href="#"></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?php echo constant('URL'); ?>consulta">Consulta</a>
+          <a class="nav-link text-light" href="<?php echo constant('URL'); ?>consulta">Consulta</a>
         </li>
 
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Lista
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -32,7 +32,7 @@
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-light border-2" type="submit">Search</button>
       </form>
     </div>
   </div>
--- views/login/index.php
+++ views/login/index.php
@@ -9,14 +9,15 @@
     <title>Document</title>
 </head>
 <body>
-
     <?php require 'views/header2.php';?>
-
-        <h1 class="center">Sección de Login</h1>
-
+    <div class="container">
+    <div class="row">
+        <div class="col-sm">
+            <h1>Login</h1>
+        </div>
+    </div>
+    </div>
     <?php require 'views/footer2.php';?>
-
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </body>
 </html>

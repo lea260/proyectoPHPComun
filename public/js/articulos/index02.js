9f28485e53a9e63c06c513c3d1f960fb6e85e8ee
Leandro Calvetti <lcalvetti1@gmail.com>
2021-06-10 17:38:01 -0300

    funcionando el borrado

--- public/js/articulos/index02.js
+++ public/js/articulos/index02.js
@@ -1,23 +1,42 @@
 (function($, param) {
   $(document).ready(function() {
       //alert('hola');
-      //console.log("funciona");
-      const items = $(".btnEliminar");      
-      items.each(item => {
-        //console.log(item);
-        $("body").on("click","#art"+ item ,function(){
-          let alumnoId = this.dataset.alumno;        
-          let url = $("#url").val();
+      console.log("funciona");
+      const items = document.querySelectorAll(".btnEliminar");
+      items.forEach(item => {
+        item.addEventListener("click", function(){
+          
+          let alumnoId = this.dataset.alumno;
+          console.log(alumnoId);
+          console.log(param);
+          //$("#filaart-"+alumnoId).remove();
+
           const confirm = window.confirm("Deseas eliminar el elemento?");
           if (confirm){
             console.log("entro if");
             //solitud ajax, 
-            $("#filaart-"+alumnoId).remove();
+            //$("#filaart-"+alumnoId).remove();
+            let url= $("#url").val();
+            let urlReq =url+"api260260articulos/borrar/"+alumnoId;
+            console.log("url: "+urlReq);
+            let headers = {"Content-Type":"application/json;charset=utf-8"};
+            let data = {};
+            $.ajax({
+              url: urlReq,
+              headers: headers,
+              type: 'DELETE',
+              data: data
+          })
+          .done(function (data) { console.log(data);
+            $("#filaart-"+alumnoId).remove()})
+          .fail(function (jqXHR, textStatus, errorThrown) { serrorFunction(); });
+
           } else {
             console.log("entro eslse");
           }
 
+
         });//end item click
       });//end item click items foreach  
   });  
-})(jQuery,"hola mundo");
+})(jQuery, "hola mundo");

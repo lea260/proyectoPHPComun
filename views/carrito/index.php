7aa04863205765272a026d72556c12b12c26f7b7
Leandro Calvetti <lcalvetti1@gmail.com>
2021-08-10 20:20:37 -0300

    agregado de vista .js y .css carrito

--- views/carrito/index.php
+++ views/carrito/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
+    <link href="<?php echo constant('URL'); ?>public/css/carrito/index.css" rel="stylesheet" type="text/css" media="all">
 
     <title>carrito</title>
 </head>
@@ -32,7 +32,7 @@
 
     <script src="<?php echo constant('URL'); ?>/public/js/jquery-3.6.0.min.js"></script>
     <!-- importo el javascript-->
-    <script src="<?php echo constant('URL'); ?>/public/js/articulos/listarJQ.js"></script>
+    <script src="<?php echo constant('URL'); ?>/public/js/articulos/index.js"></script>
     <!--<script src="<?php echo constant('URL'); ?>/public/js/main.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 

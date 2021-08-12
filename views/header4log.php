20096cbf7b8711112b8ac9049ecbafc038198ac9
Leandro Calvetti <lcalvetti1@gmail.com>
2021-08-12 16:30:18 -0300

    agregar cantidad items carrito

--- views/header4log.php
+++ views/header4log.php
@@ -24,7 +24,14 @@
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-light" href="<?php echo constant('URL'); ?>carrito" >Carrito</a>
+              <a class="nav-link text-light" href="<?php echo constant('URL'); ?>carrito" > <button type="button" class="btn btn-primary position-relative">
+      Carrito
+      <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+        99+
+        <span class="visually-hidden">unread messages</span>
+      </span>
+      </button>
+      </a>
         </li>
 
 

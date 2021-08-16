63477cf85f9c284d2fc0f5a992ab8fbb93ef2856
Leandro Calvetti <lcalvetti1@gmail.com>
2021-08-16 20:11:59 -0300

    agregado de jwt

--- controllers/Login_Controller.php
+++ controllers/Login_Controller.php
@@ -1,4 +1,6 @@
 <?php
+require 'vendor/autoload.php';
+use Firebase\JWT\JWT;
 
 class Login_Controller extends Controller
 {
@@ -19,6 +21,34 @@ class Login_Controller extends Controller
 
     public function ingresar()
     {
+        $key = "example_key";
+        $payload = array(
+            "iss" => "http://example.org",
+            "aud" => "http://example.com",
+            "iat" => 1356999524,
+            "nbf" => 1357000000,
+        );
+        $jwt = JWT::encode($payload, $key);
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
+
+        print_r($jwt);
+
+/*
+NOTE: This will now be an object instead of an associative array. To get
+an associative array, you will need to cast it as such:
+ */
+
+        $decoded_array = (array) $decoded;
+
+/**
+ * You can add a leeway to account for when there is a clock skew times between
+ * the signing and verifying servers. It is recommended that this leeway should
+ * not be bigger than a few minutes.
+ *
+ * Source: http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html#nbfDef
+ */
+        JWT::$leeway = 60; // $leeway in seconds
+        $decoded = JWT::decode($jwt, $key, array('HS256'));
         //$alumnos = $this->model->get();
         //$this->view->alumnos = "exitoso";
         //$this->view->post = var_dump($_POST);
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'f2d940065d1242102e4c4b05be1d3c2ebbf8406d',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'f2d940065d1242102e4c4b05be1d3c2ebbf8406d',
+            'dev_requirement' => false,
+        ),
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v5.4.0',
+            'version' => '5.4.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'reference' => 'd2113d9b2e0e349796e72d2a63cf9319100382d2',
+            'dev_requirement' => false,
+        ),
+    ),
+);

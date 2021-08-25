f4b0e5e5a84fb7664dc1d2091bf5e91f991e945a
Nicolas Gomez <nexo30bb@hotmail.com>
2021-08-25 01:27:35 -0300

    agregado vista y controlador

---
+++ controllers/Apicarrito_Controller.php
@@ -0,0 +1,31 @@
+<?php
+require_once 'entidades/articulo.php';
+require_once 'vendor/autoload.php';
+
+class Apicarrito_Controller extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    //localahost/prophp3bj/proyectoPHPComun/Api260260articulos
+    public function completarcarrito()
+    {
+
+        $listaArticulos = [];
+        $respuesta = [
+            "datos" => $listaArticulos,
+            "totalResultados" => count($listaArticulos),
+        ];
+        $this->view->respuesta = json_encode($respuesta);
+
+        $this->view->render('api/completarcarrito');
+        //var_dump($this);
+        //var_dump($this->view);
+        //$this->view->render('apilea/articulos/index');
+        //var_dump($this);
+        //var_dump($this->view);
+    }
+}
--- public/js/carrito/index.js
+++ public/js/carrito/index.js
@@ -3,64 +3,36 @@
       //alert('carrito');
       let carrito = JSON.parse(localStorage.getItem("carrito"));
       //$("#carrito").text(carrito.length);
+      console.log(carrito);
       carrito.forEach(element => {
-        //let insert = `<button type="button" class="btn btn-primary">${element.id}</button>` 
-        let insert02 = `<div class="col-lg-4 col-md-6 col-sm-6 col-xs-4 p-3"
-        id="art-${element.id}">
+        let insert = `<button type="button" class="btn btn-primary">${element.id}</button>` 
+        let insert02 = `<div class="col-lg-4 col-md-6 col-sm-6 col-xs-4 p-3">
         <div class="card">
-          <img class="card-img-top" src="${element.url}" alt="Card image cap"/>
+          <img class="card-img-top" src="<?=$value->url;?>" alt="Card image cap"/>
           <div class="card-body">
             <h5 class="card-title">ID:${element.id} ${element.codigo}</h5>
             <p class="card-text">${element.descripcion}</p>
             <p class="card-text">$ ${element.precio}</p>
-            <input id="cant-${element.id}" class="form-control"
-            value="${element.cantidad}" type="number" disabled
-            ></p>
-            <button type="button" class="btn btn-danger btnEliminar"
-            data-articulo-id="${element.id}"
-                        >Eliminar</button>
+            <input value="${element.cantidad}" class="form-control"
+            type="number" disabled>
+            </p>
+            <button type="button" class="btn btn-primary btnEliminar"
+            data-articulo-id="<?php echo $value->id; ?>"
+            data-articulo-descripcion="<?php echo $value->descripcion; ?>"
+            data-articulo-codigo="<?php echo $value->codigo; ?>"
+            >Eliminar</button>
           </div>
           </div><!-- end card -->
         </div><!-- end col --><?php }`
         $("#carritoid").after(insert02);
       });
       /*for (let index = 0; index < array.length; index++) {
-
         
         
       }*/
-      $("body").on("click",".btnEliminar" ,function(){
-        console.log("entro");
-        let articuloId= $(this).data("articuloId");
-        //console.log(articuloId);
-        const confirm = window.confirm("Deseas eliminar el elemento?");
-        if (confirm){
-          $("#art-"+articuloId).remove();
-          //actualizar
-          //localStorage.setItem("carrito", JSON.stringify(carrito));
-          let carritoStr = localStorage.getItem("carrito");
-          if (carritoStr){
-            //console.log(carritoStr);
-            console.log("-----------------");
-            let carrito= JSON.parse(carritoStr);
-            console.log(carrito);
-            let itemCarrito= carrito.find(articulo => articulo.id ==articuloId);
-            carrito.forEach(function(art, index, object) {
-              if(art.id == articuloId){
-                object.splice(index, 1);
-                localStorage.setItem("carrito", JSON.stringify(carrito));
-                //console.log("probando");
-                
-                
-                
-              }
-              $("#cantidadElemCarrito").text(carrito.length);
-          });
-          //$("#cantidadElemCarrito").text(carrito.length);
-          
-           
-          }
-        }
+      $("body").on("click",".btnEliminar", 
+      function(){
+        $("#parrafo5").after('<button type="button" class="btnHola">Click Me!</button>');
 
       });
       

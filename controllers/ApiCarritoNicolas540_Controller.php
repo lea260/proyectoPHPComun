5f2d0c192fd7840b317129ac6e0db117a44ab468
eric <eg492391@gmail.com>
2021-08-25 01:39:27 -0300

    Agregar carrito

---
+++ controllers/ApiCarritoNicolas540_Controller.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'entidades/articulo.php';
+require_once 'vendor/autoload.php';
+
+class ApiCarritoNicolas540_Controller extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    //localahost/prophp3bj/proyectoPHPComun/Api260260articulos
+    public function completarcarrito()
+    {
+
+        $listaArticulos = [];
+        $respuesta = [
+            "datos" => $listaArticulos,
+            "totalResultados" => count($listaArticulos),
+        ];
+        $this->view->respuesta = json_encode($respuesta);
+
+        $this->view->render('api/carrito/completarcarrito');
+        //var_dump($this);
+        //var_dump($this->view);
+        //$this->view->render('apilea/articulos/index');
+        //var_dump($this);
+        //var_dump($this->view);
+    }
+
+}

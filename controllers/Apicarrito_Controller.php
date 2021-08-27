e5a501e5dca03ec4d44e9368a2b23e1f1f38eb21
Nicolas Gomez <nexo30bb@hotmail.com>
2021-08-26 21:10:33 -0300

    Agregado crear lista carrito

--- controllers/Apicarrito_Controller.php
+++ controllers/Apicarrito_Controller.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'entidades/articulo.php';
+require_once 'entidades/carrito.php';
 require_once 'vendor/autoload.php';
 
 class Apicarrito_Controller extends Controller
@@ -13,10 +13,20 @@ class Apicarrito_Controller extends Controller
     //localahost/prophp3bj/proyectoPHPComun/Api260260articulos
     public function completarcarrito()
     {
-
-        $listaArticulos = [];
+        $json = file_get_contents('php://input');
+        //convierto en un array asociativo de php
+        $listaArticulos = json_decode($json);
+        $lista = [];
+        foreach ($listaArticulos as $key => $obj) {
+            $articulo = new Carrito();
+            $articulo->id = $obj->id;
+            $articulo->cantidad = $obj->cantidad;
+            $articulo->precio = $obj->precio;
+            //$lista[] = $articulo;
+            array_push($lista, $articulo);
+        }
         $respuesta = [
-            "datos" => $listaArticulos,
+            "datos" => $lista,
             "totalResultados" => count($listaArticulos),
         ];
         $this->view->respuesta = json_encode($respuesta);

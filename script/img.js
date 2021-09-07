86e1bb4664eefa4de4a97e6345ccfb8b27602bee
Leandro Calvetti <lcalvetti1@gmail.com>
2021-09-07 17:58:47 -0300

    previsualizar imagen

---
+++ script/img.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("input").change(function(e){
+    var let = e.target;
+    var let = new FileReader();
+    reader.onload = function(){
+      var dataURL = reader.result;
+      console.log(dataURL);
+      //var output = document.getElementById('output');
+      //output.src = dataURL;
+      $("#imgP").html("<img src='" +dataURL+ "' />");
+      $("#imgP").html(`<img src="${dataURL}" />`);
+    };
+    reader.readAsDataURL(input.files[0]);
+  });
+});
+</script>
+</head>
+<body>
+
+<input type="file">
+<div id="imgP"><div/>
+
+<p>Write something in the input field, and then press enter or click outside the field.</p>
+
+
+</body>
+</html>
